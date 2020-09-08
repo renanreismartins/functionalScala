dff2f6a6a76e5ea03207662617c51a5ec5782863
Renan Reis <renanreismartins@gmail.com>
2020-09-08 13:31:55 +0200

    Exe 2.4 Implement uncurry, which reverses the transformation of curry.

--- src/main/scala/chap02/Chap02.scala
+++ src/main/scala/chap02/Chap02.scala
@@ -12,6 +12,10 @@ object Chap02 {
     (a: A) => (b: B) => f.apply(a, b)
   }
 
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
+
   def main(args: Array[String]): Unit = {
     println(isSorted(Array(1,2), (a: Int, b: Int) => a < b))
   }
