b70d23f684291eb857bfa5bbbd54f000a4252149
Renan Reis <renanreismartins@gmail.com>
2020-09-08 12:11:53 +0200

    Chap 02 - isSorted with tailRec

---
+++ src/main/scala/chap02/Exe2_2.scala
@@ -0,0 +1,14 @@
+package chap02
+
+object Exe2_2 {
+  @annotation.tailrec
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    if (as.length < 2) true
+    else if (ordered(as(0), as(1))) isSorted(as.tail, ordered)
+    else false
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(isSorted(Array(1,2), (a: Int, b: Int) => a < b))
+  }
+}
