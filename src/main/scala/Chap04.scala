44b2c317d36c6d88f0a1ae6ca81c4376b09fe1c8
Renan Reis <renanreismartins@gmail.com>
2020-09-14 22:25:40 +0200

    Exercise 4.3 - Write a generic function map2 that combines two Option values using a binary function.

--- src/main/scala/Chap04.scala
+++ src/main/scala/Chap04.scala
@@ -37,6 +37,8 @@ object Chap04 {
     mean(xs).flatMap(m => mean(xs.map(e => math.pow(e - m, 2))))
   }
 
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a.flatMap(va => b.map(vb => f(va, vb)))
+
   def main(args: Array[String]): Unit = {
     println("Map")
     println(Some("Renan").map(_.toUpperCase))
