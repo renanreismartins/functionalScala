eb06f74024e1adddf2819f7d40a1c919c10a854e
Renan Reis <renanreismartins@gmail.com>
2020-09-18 16:51:18 +0200

    Exercise 5.2 - Write the function take(n) for returning the first n elements of a Stream, and drop(n) for skipping the first n elements of a Stream.

--- src/main/scala/Chap05.scala
+++ src/main/scala/Chap05.scala
@@ -1,3 +1,5 @@
+import Chap05.Stream.cons
+
 object Chap05 {
 
   sealed trait Stream[+A] {
@@ -8,6 +10,20 @@ object Chap05 {
       }
 
       loop(this, Nil).reverse
+    }
+
+    def take(n: Int): Stream[A] = {
+      def loop(s: Stream[A], acc: Stream[A], i: Int): Stream[A] = s match {
+        case Empty => acc
+        case Cons(h, t) => if (i > 0) cons(h(), loop(t(), acc, i - 1)) else acc
+      }
+
+      loop(this, Empty, n)
+    }
+
+    def drop(n: Int): Stream[A] = this match {
+      case Empty => Empty
+      case Cons(_, t) => if (n == 0) this else t().drop(n -1)
     }
   }
 
@@ -33,6 +49,14 @@ object Chap05 {
   def main(args: Array[String]): Unit = {
     println("toList")
     println(Stream.cons(1, Stream.cons(2, Stream.empty)).toList)
+    println()
+
+    println("take")
+    println(Stream.cons(1, Stream.cons(2, Stream.empty)).take(2).toList)
+    println()
+
+    println("drop")
+    println(Stream.cons(1, Stream.cons(2, Stream.empty)).drop(1).toList)
     println()
   }
 }
