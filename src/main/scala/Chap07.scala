105956ba35aaf9f1a0d2054bf8c06f4813cfac2e
Renan Reis <renanreismartins@gmail.com>
2020-10-12 15:20:30 +0200

    Exercise 7.4 - using lazyUnit, write a function to convert any function A => B to one that evaluates its result asynchronously.

---
+++ src/main/scala/Chap07.scala
@@ -0,0 +1,51 @@
+import java.util.concurrent._
+
+object Chap07 {
+
+  object Par {
+    type Par[A] = ExecutorService => Future[A]
+
+    def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+    def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+    private case class UnitFuture[A](get: A) extends Future[A] {
+      def isDone = true
+      def get(timeout: Long, units: TimeUnit) = get
+      def isCancelled = false
+      def cancel(evenIfRunning: Boolean): Boolean = false
+    }
+
+    def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] =
+      (es: ExecutorService) => {
+        val af = a(es)
+        val bf = b(es)
+        UnitFuture(f(af.get, bf.get))
+      }
+
+    def fork[A](a: => Par[A]): Par[A] =
+      es => es.submit(new Callable[A] {
+        def call = a(es).get
+      })
+
+    def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+    def asyncF[A,B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
+
+  }
+
+  def sum(ints: IndexedSeq[Int]): Int =
+    if (ints.size <= 1)
+      ints.headOption getOrElse 0
+    else {
+      val (l, r) = ints.splitAt(ints.length / 2)
+      sum(l) + sum(r)
+    }
+
+
+
+  def main(args: Array[String]): Unit = {
+    println(sum(IndexedSeq(1, 2, 3)))
+  }
+
+}
