20fd090a7d70b7a99eaa073f95553560512d2979
Renan Reis <renanreismartins@gmail.com>
2020-10-20 18:31:02 +0200

    Init chap 08

---
+++ src/main/scala/Chap08.scala
@@ -0,0 +1,35 @@
+import Chap05.Stream
+import java.util.concurrent.{ExecutorService, Executors}
+
+import Chap08.{Gen, Prop}
+
+object Chap08 {
+
+
+  /*
+  The library developed in this chapter goes through several iterations. This file is just the
+  shell, which you can fill in and modify while working through the chapter.
+  */
+
+  trait Prop {
+  }
+
+  object Prop {
+    def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
+  }
+
+  object Gen {
+    def unit[A](a: => A): Gen[A] = ???
+  }
+
+  trait Gen[A] {
+    def map[A, B](f: A => B): Gen[B] = ???
+
+    def flatMap[A, B](f: A => Gen[B]): Gen[B] = ???
+  }
+
+  trait SGen[+A] {
+
+  }
+
+}
