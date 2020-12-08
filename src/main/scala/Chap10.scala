76b7520f192c9a9c1b4874345eaa19e957e0ab5d
Renan Reis <renanreismartins@gmail.com>
2020-12-08 21:49:02 +0100

    Exercise 10.2 Give a Monoid instance for combining Option values.

--- src/main/scala/Chap10.scala
+++ src/main/scala/Chap10.scala
@@ -40,4 +40,10 @@ object Monoid {
 
     override def zero: Boolean = false
   }
+
+  def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    override def op(a1: Option[A], a2: Option[A]): Option[A] = a1.orElse(a2)
+
+    override def zero: Option[A] = None
+  }
 }
