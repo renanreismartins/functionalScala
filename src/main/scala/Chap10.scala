556579f3ebc10e10808d2ce0f47be998afad91cd
Renan Reis <renanreismartins@gmail.com>
2020-12-08 21:45:56 +0100

    Chap 10 monoids skaffold

---
+++ src/main/scala/Chap10.scala
@@ -0,0 +1,27 @@
+trait Monoid[A] {
+  def op(a1: A, a2: A): A
+  def zero: A
+}
+
+object Monoid {
+
+  val stringMonoid = new Monoid[String] {
+    def op(a1: String, a2: String) = a1 + a2
+
+    val zero = ""
+  }
+
+  def listMonoid[A] = new Monoid[List[A]] {
+    def op(a1: List[A], a2: List[A]) = a1 ++ a2
+
+    val zero = Nil
+  }
+
+  val intAddition: Monoid[Int] = ???
+
+  val intMultiplication: Monoid[Int] = ???
+
+  val booleanOr: Monoid[Boolean] = ???
+
+  val booleanAnd: Monoid[Boolean] = ???
+}
